32ddf4142affff1ec33696382f94798aa77d7ba8
George Kibe <georgekibewambui@gmail.com>
2022-12-03 15:04:23 +0300

    navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products"
 
+import "./App.scss"
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<Home/>
+    element:<Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/products/:id",
+        element:<Products/>
+      },
+      {
+        path:"/product/:id",
+        element:<Product/>
+      },
+    ]
   },
-  {
-    path:"/products/:id",
-    element:<Product/>
-  },
-  {
-    path:"/product/:id",
-    element:<Products/>
-  },
+  
 ])
+
+
 function App() {
   return (
     <div>
