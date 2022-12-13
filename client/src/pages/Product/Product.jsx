c791556617be299993dd62a570e72f99a1d0cc9a
George Kibe <georgekibewambui@gmail.com>
2022-12-13 19:10:40 +0300

    initializing product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 import "./Product.scss"
+
 function Product() {
+  const [selectedImage, setSelectedImage] = useState(0)
+
+  const images = [
+    "https://i.ibb.co/h79LrJD/T-Shirt.jpg",
+    "https://i.ibb.co/nnH3WgQ/placeholder4.jpg"
+  ]
+
   return (
-    <div>Product</div>
+    <div className='product'>
+      <div className="lefty">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={e => setSelectedImage(0)} />
+          <img src={images[1]} alt="" onClick={e => setSelectedImage(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImage]} alt="" />
+        </div>
+      </div>
+      <div className="right">
+
+      </div>
+    </div>
   )
 }
 
