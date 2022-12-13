5b2faa9b5e4c16fd275e1f9108693f81c5358dc1
George Kibe <georgekibewambui@gmail.com>
2022-12-13 18:56:03 +0300

    products scss

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -8,28 +8,28 @@ const List = () => {
         id:1,
         img:"https://i.ibb.co/NFfj5k7/shoes.jpg",
         title:"Shoes",
-        oldprice:12435,
+        oldPrice:12435,
         price:34628
     },
     {
         id:2,
         img:"https://i.ibb.co/h79LrJD/T-Shirt.jpg",
         title:"Shirts",
-        oldprice:12435,
+        oldPrice:12435,
         price:34628
     },
     {
         id:3,
         img:"https://i.ibb.co/NFfj5k7/shoes.jpg",
         title:"Shoes",
-        oldprice:12435,
+        oldPrice:12435,
         price:34628
     },
     {
         id:4,
         img:"https://i.ibb.co/h79LrJD/T-Shirt.jpg",
         title:"Shirts",
-        oldprice:12435,
+        oldPrice:12435,
         price:34628
     },
   ]
