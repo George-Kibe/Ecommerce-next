32b94515f013786eb009ca0c1491895157b9d800
George Kibe <georgekibewambui@gmail.com>
2022-12-27 15:02:01 +0300

    product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart"
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder"
-import Balanceicon from "@mui/icons-material/Balance"
+import BalanceIcon from "@mui/icons-material/Balance"
 
 import "./Product.scss"
 
@@ -27,7 +27,7 @@ function Product() {
       </div>
       <div className="right">
         <h1>Product Title</h1>
-        <span>Kshs. 2500</span>
+        <span className='price'>Kshs. 2500</span>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, repudiandae. Quidem aut sapiente quae ipsam dolorum hic eaque quasi repellat asperiores et quas, cum quibusdam consectetur qui corporis eos excepturi totam in veniam enim earum nemo. Quos eligendi iusto, repellendus fugiat aut sit ipsam soluta aspernatur, similique dolorem nam quam sequi deserunt, unde cum id quod molestias necessitatibus dolorum magni perspiciatis fuga. Laboriosam asperiores voluptate, iure porro quis ab officia fuga temporibus eaque obcaecati beatae nesciunt modi repudiandae dignissimos perspiciatis placeat perferendis aperiam molestias maxime voluptatum pariatur itaque, minus sed odio? Aliquam alias quod nesciunt, libero vel tempora ipsa adipisci error architecto veniam, molestiae id illum voluptas pariatur deserunt dicta facere, molestias corporis dolorem consequuntur velit ea. Dolores quia dolorem suscipit minima corrupti libero ratione nostrum amet dolorum harum cumque unde temporibus consequatur commodi sequi dicta ab, praesentium est mollitia dignissimos quae perferendis? Laudantium voluptate inventore, animi vitae itaque ex, sed beatae ullam ea similique modi placeat commodi! Quidem, assumenda dolor repellendus laborum aperiam dicta voluptas tenetur explicabo corporis maxime error debitis at repellat deleniti corrupti quaerat nisi! Quod enim quos, neque totam earum magni laudantium ducimus ab, nihil sit voluptas sint ipsum et explicabo officiis a repellendus laborum nostrum.
         </p>
@@ -39,6 +39,27 @@ function Product() {
         <button className="add">
           <AddShoppingCartIcon /> ADD TO CART
         </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon/> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon/> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor:Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQs</span>
+        </div>
       </div>
     </div>
   )
