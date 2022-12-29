6f560c80fb1adc4190cabe323eb8bae7c1d1584e
George Kibe <georgekibewambui@gmail.com>
2022-12-29 08:46:42 +0300

    database setup

--- api/config/database.js
+++ api/config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 3306),
       database: env('DATABASE_NAME', 'store'),
       user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'kibe1234'),
+      password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
