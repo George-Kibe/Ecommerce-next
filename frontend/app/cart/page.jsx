709f6fc4ad6f8506952179262de69b50a5fd8266
George-Kibe <georgekibewambui@gmail.com>
2023-06-29 16:48:20 +0300

    footer fixed

--- frontend/app/cart/page.jsx
+++ frontend/app/cart/page.jsx
@@ -143,7 +143,7 @@ export default function CartPage() {
   }
   return (
     <>
-      {/* <ToastContainer /> */}
+      <ToastContainer /> 
       <Center>
         <ColumnsWrapper>
           <Box>
